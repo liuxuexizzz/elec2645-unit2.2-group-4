1fcb063061088562bd4a331bf4f75eff0450eccf
nana1291590712 <162336176+nana1291590712@users.noreply.github.com>
2024-11-20 13:11:50 +0800

    Update input_functions.h

--- input_functions.h
+++ input_functions.h
@@ -1,9 +1,12 @@
+#ifndef INPUT_FUNCTIONS_H
+#define INPUT_FUNCTIONS_H
+
 #include <string>
 
 // Function declarations
-double getDoubleInput();
-int getIntInput();
-std::string getStringInput();
-char getCharInput();
+double getDoubleInput();    // Function to get a double input
+int getIntInput();          // Function to get an integer input
+std::string getStringInput(); // Function to get a string input
+char getCharInput();        // Function to get a character input
 
 #endif // INPUT_FUNCTIONS_H
