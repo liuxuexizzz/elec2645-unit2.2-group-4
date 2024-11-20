4696e271f898098eda29039023fd1deebf3aef00
nana1291590712 <162336176+nana1291590712@users.noreply.github.com>
2024-11-20 13:17:33 +0800

    Create ohms_calculator.h

---
+++ ohms_calculator.h
@@ -0,0 +1,9 @@
+#ifndef OHMS_CALCULATOR_H
+#define OHMS_CALCULATOR_H
+
+#include <string>
+
+// Function declarations
+void calculateOhmsLaw();      // Main calculator function
+
+#endif // OHMS_CALCULATOR_H
