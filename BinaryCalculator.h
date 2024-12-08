039a08771aa9be05680e636aa0013bf8ca925e94
liuxuexizzz <140696788+liuxuexizzz@users.noreply.github.com>
2024-12-08 19:37:17 +0800

    Update BinaryCalculator.h

--- BinaryCalculator.h
+++ BinaryCalculator.h
@@ -4,13 +4,13 @@
 #include <string>
 
 
-std::string binaryAnd(const std::string& bin1, const std::string& bin2);        // 按位与操作
-std::string binaryOr(const std::string& bin1, const std::string& bin2);         // 按位或操作
-std::string binaryNot(const std::string& bin);                                  // 按位非操作
-std::string binaryNand(const std::string& bin1, const std::string& bin2);       // 按位NAND操作
-std::string binaryNor(const std::string& bin1, const std::string& bin2);        // 按位NOR操作
-std::string binaryXor(const std::string& bin1, const std::string& bin2);        // 按位XOR操作
-std::string binaryXnor(const std::string& bin1, const std::string& bin2);       // 按位XNOR操作
-bool isBinary(const std::string& str);                                          // 检测字符串是否为二进制数
+std::string binaryAnd(const std::string& bin1, const std::string& bin2);        // Bitwise AND operation
+std::string binaryOr(const std::string& bin1, const std::string& bin2);         // Bitwise OR operation
+std::string binaryNot(const std::string& bin);                                  // Bitwise NOT operation
+std::string binaryNand(const std::string& bin1, const std::string& bin2);       // Bitwise NAND operation
+std::string binaryNor(const std::string& bin1, const std::string& bin2);        // Bitwise NOR operation
+std::string binaryXor(const std::string& bin1, const std::string& bin2);        // Bitwise XOR operation
+std::string binaryXnor(const std::string& bin1, const std::string& bin2);       // Bitwise XNOR operation
+bool isBinary(const std::string& str);                                          // Check if a string is a binary number.
 
 #endif // BINARY_CALCULATOR_H
